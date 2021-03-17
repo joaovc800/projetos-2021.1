232df47b8d4126e5cc887c8d9e94edc27451996d
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-03-17 19:51:38 -0300

    Create planos.php

---
+++ planos.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Wit Solutions | Planos</title>
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="Wit Solutions empresa de web segurança, sua empresa está segura?">
+    <meta property="og:image" content="logo1.0.png">
+    <meta property="og:description" content="Wit Solutions empresa de web segurança, sua empresa está segura?">
+    <meta property="og:title" content="Wit Solutions">
+    <link rel="stylesheet" href="planos.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="logo2.0.png"/>
+  </head>
+  <body>
+    <header class="sticky-top">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid menu">
+          <a class="navbar-brand" href="index.php" title="Home">Wit Solutions</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Sobre</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Contato</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Serviços e suporte</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="planos.php">Planos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">COVID-19</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+    <main class="container">
+      <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+        <h1 class="display-4">Preços</h1>
+        <p class="lead">Confira aqui os melhores preços e beneficios do melhor site de web segurança. Temos sempre o melhor plano para você e sua empresa, lembre-se se seu negocio ou ação não estiver seguro contrate já! Wit Solutions cada solução é única.</p>
+      </div>
+      <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+        <div class="col">
+          <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 fw-normal">Community</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$50 <small class="text-muted">/ dólar</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Suporte 24 horas</li>
+              <li>Firewall</li>
+              <li>Suporte por e-mail</li>
+              <li>central de antedimento</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Compre agora</button>
+          </div>
+        </div>
+        </div>
+        <div class="col">
+          <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 fw-normal">Pro</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$120 <small class="text-muted">/ dólar</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Suporte 24 horas</li>
+              <li>Firewall</li>
+              <li>Data Center</li>
+              <li>Hack e switch</li>
+              <li>Suporte por e-mail</li>
+              <li>Central de atendimento</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-primary">Compre agora</button>
+          </div>
+        </div>
+        </div>
+        <div class="col">
+          <div class="card mb-4 shadow-sm">
+          <div class="card-header">
+            <h4 class="my-0 fw-normal">Enterprise</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$200 <small class="text-muted">/ dólar</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Suporte 24 horas</li>
+              <li>Firewall</li>
+              <li>Data Center</li>
+              <li>Hack e switch</li>
+              <li>Instalação grátis</li>
+              <li>Softwares de monitoramento</li>
+              <li>Suporte por e-mail</li>
+              <li>Central de atendimento</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-primary">Compre agora</button>
+          </div>
+        </div>
+        </div>
+      </div>
+    </main>
+      <div class="container-fluid" style="background-color: #293d55">
+        <footer class="container py-3" style="background-color: #293d55">
+          <div class="row">
+            <div class="col-6 col-md">
+              <h5 class="text-light">Features</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="link-secondary" href="#">Segurança</a></li>
+                <li><a class="link-secondary" href="#">Web</a></li>
+                <li><a class="link-secondary" href="#">Firewall</a></li>
+                <li><a class="link-secondary" href="#">Para desenvolvedores</a></li>
+                <li><a class="link-secondary" href="#">Data center</a></li>
+                <li><a class="link-secondary" href="#">Link up</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5 class="text-light">Resources</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="link-secondary" href="#">Planos</a></li>
+                <li><a class="link-secondary" href="#">Contatos</a></li>
+                <li><a class="link-secondary" href="#">Sobre</a></li>
+                <li><a class="link-secondary" href="#">Compras</a></li>
+              </ul>
+              </div>
+              <div class="col-6 col-md">
+                <h5 class="text-light">About</h5>
+                <ul class="list-unstyled text-small">
+                  <li><a class="link-secondary" href="#">Equipe</a></li>
+                  <li><a class="link-secondary" href="#">Localizações</a></li>
+                  <li><a class="link-secondary" href="#">Privacidade</a></li>
+                  <li><a class="link-secondary" href="#">Termos</a></li>
+                </ul>
+              </div>
+              <div class="col-6 col-md d-inline">
+                <h5 class="text-light">Redes Sociais</h5>
+                <ul class="list-unstyled text-small">
+                  <li><a class="link-secondary center" href="#" title="Facebook"><img src="facebook.png" class="icon"></a></li>
+                  <li><a class="link-secondary center" href="#" title="Instagram"><img src="instagram.png" class="icon"></a></li>
+                  <li><a class="link-secondary center" href="#" title="Whatsapp"><img src="wpp.png" class="icon"></a></li>
+                  <li><a class="link-secondary center" href="#" title="Twitter"><img src="twitter.png" class="icon"></a></li>
+                </ul>
+              </div>
+          </div>
+      </div>
+      <div class="container-fluid bg-dark">
+        <div class="container">
+          <p class="text-light text-center p-3">Wit Solutions CRM. | CNPJ 54.321.458/0001-03</p>
+          <p class="text-light text-center p-3">Av. Tietê 2141, Torre E 10º andar - São paulo</p>
+          <p class="text-light text-center p-3">Formas de pagamento aceitas: cartões de crédito (Visa, MasterCard, Elo e American Express) e boleto.</p>
+          <div class="container">
+            <hr class="featurette-divider bg-light">
+          </div>
+          <p class="text-light text-center py-3">&copy 2020, todos os direitos reservados</p>
+        </div>
+      </div>
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
