cc9e001214c92b99eb9419d023df6953b1468c17
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-03-17 18:42:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -8,13 +8,13 @@
     <meta property="og:image" content="logo1.0.png">
     <meta property="og:description" content="Wit Solutions empresa de web segurança, sua empresa está segura?">
     <meta property="og:title" content="Wit Solutions">
-    <link rel="stylesheet" href="home.css"  />
+    <link rel="stylesheet" href="home.css"/>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <script src="script.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link rel="shortcut icon" href="logo2.0.png" />
+    <link rel="shortcut icon" href="logo2.0.png"/>
   </head>
   <body>
     <header class="sticky-top">
