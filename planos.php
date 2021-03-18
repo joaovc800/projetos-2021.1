34daaf66e0eab1dd839ca1f26cb80b98fb3f2bff
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-03-17 21:14:52 -0300

    Update planos.php

--- planos.php
+++ planos.php
@@ -20,7 +20,7 @@
     <header class="sticky-top">
       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid menu">
-          <a class="navbar-brand" href="index.php" title="Home">Wit Solutions</a>
+          <a class="navbar-brand" href="index.php" title="Home"><img src="sec.png"></a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
           </button>
