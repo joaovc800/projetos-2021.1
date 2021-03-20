0b4feaa27f3479fac15687ef8d7eae27a8cdb676
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-03-20 11:13:48 -0300

    Update contato.php
    inclusão da API do chat online

--- contato.php
+++ contato.php
@@ -106,6 +106,19 @@
           <div class="container">
             <span class="text-muted">Central de atendimento  (0800-889-4000)</span><br>
             <span class="text-muted">E-mail para contato (witsolutions.crm@outlook.com)</span>
+            <!--Start of Tawk.to Script-->
+            <script type="text/javascript">
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/6055fd65f7ce182709321f12/1f17ubljp';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+            </script>
+            <!--End of Tawk.to Script-->
           </div>
         </footer> 
       <div class="container-fluid" style="background-color: #293d55">
