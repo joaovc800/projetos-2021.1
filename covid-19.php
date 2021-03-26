94dac394c903737239795a23dad5f01f9826e878
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-03-26 13:49:51 -0300

    Create covid-19.php
    criação da página covid-19

---
+++ covid-19.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Wit Solutions | Covid-19</title>
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="Wit Solutions empresa de web segurança, sua empresa está segura?">
+    <meta property="og:image" content="logo1.0.png">
+    <meta property="og:description" content="Wit Solutions empresa de web segurança, sua empresa está segura?">
+    <meta property="og:title" content="Wit Solutions">
+    <link rel="stylesheet" href="planos.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="logo2.0.png"/>
+  </head>
+  <body>
+    <header class="sticky-top">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid menu">
+          <a class="navbar-brand" href="index.php" title="Home"><img src="sec.png"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="sobre.php">Sobre</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="contato.php">Contato</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Serviços e suporte</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="planos.php">Planos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="covid-19.php">COVID-19</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+      <main class="container">
+          <div class="p-4 p-md-5 mt-4 mb-2 text-white rounded bg-dark">
+            <div class="col-5 px-0">
+              <h1 class="display-4 fst-italic">Notícias sobre COVID-19</h1>
+              <p class="lead my-3">Estamos vivendo novas fases da Covid-19, com abertura de estabelecimentos, necessidade de sair para trabalhar e/ou interagir. A pandemia não acabou, mas precisamos entender quais os melhores modos de nos proteger e proteger os outros nas próximas etapas.</p>
+            </div>
+          </div>
+
+          <div class="row mb-2">
+            <div class="col-md-6">
+              <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                  <strong class="d-inline-block mb-2 text-primary">Antiga</strong>
+                  <h3 class="mb-0">Variante do COVID-19</h3>
+                  <div class="mb-1 text-muted">janeiro 12</div>
+                  <p class="card-text mb-auto">Nota informativa: a variante da COVID-19 e o impacto sobre os testes moleculares.</p>
+                  <a href="#" class="stretched-link">Ler mais</a>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+                  <img class="bd-placeholder-img h-100" src="covid.jpg">
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                <div class="col p-4 d-flex flex-column position-static">
+                  <strong class="d-inline-block mb-2 text-success">Mais recente</strong>
+                  <h3 class="mb-0">Fechando as portas</h3>
+                  <div class="mb-1 text-muted">Março 11</div>
+                  <p class="mb-auto">Lockdown durante a pandemia do Coronavírus: o que é e quais países adotaram.</p>
+                  <a href="#" class="stretched-link">Ler mais</a>
+                </div>
+                <div class="col-auto d-none d-lg-block">
+                  <img class="bd-placeholder-img h-100" src="lockdown.jpg">
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-8">
+              <h3 class="pb-4 mb-4 fst-italic border-bottom">
+                Informações verídicas
+              </h3>
+
+              <article class="blog-post">
+                <h2 class="blog-post-title">1 Ano sem encontrar o tratamento de COVID-19</h2>
+                <p class="blog-post-meta">Março 13, 2021</p>
+
+                <p>Um ano atrás a OMS declarou que a COVID-19 passou ao status de pandemia. Desde então, cientistas ao redor do mundo estão em busca de um tratamento específico para essa doença, sem muito sucesso até o momento.</p>
+                <hr>
+                <p>No começo, a principal estratégia de busca utilizada foi o reposicionamento de fármacos. Assim, essa alternativa utiliza remédios já aprovados para uso em certas doenças, e tenta tratar novas doenças com o mesmo remédio. Por serem remédios conhecidos, já existem diversos estudos de como agem no corpo, e quais doses são seguras de serem tomadas. Dessa forma, eles necessitam de menos investimento de tempo e dinheiro para as pesquisas com as novas doenças, como a COVID-19. Contudo, a “pressa” para encontrar uma resposta contra a pandemia provocou a disseminação de algumas informações equivocadas que acabaram sendo adotadas como verdade absoluta por parte da população.</p>
+                <blockquote>
+                  <p>Boa parte dos experimentos iniciais realizados com esses remédios foram feitos em um sistema in vitro. Ou seja, isso significa que os cientistas cultivaram células em pequenas placas, e fizeram os testes. Em situações normais, depois que os testes in vitro são feitos, os remédios acabam em uma etapa de testes em animais. Esses testes se fazem necessários. Pois, diferente de um sistema in vivo, ou seja, em um animal, os experimentos em culturas de células são muito controlados e não representam toda a complexidade que um corpo possui. Assim, muitas coisas podem apresentar resultados promissores nos testes com células, mas falham quando testados para os animais.<br><br>
+
+                  No contexto da Pandemia, esses remédios de testes de reposicionamento podem pular a etapa de segurança e acabam nos estudos clínicos com humanos. Afinal já conhecia-se a relação de segurança do remédio. Só não sabíamos se ele funcionava para COVID-19. Dois grandes estudos clínicos foram realizados, o SOLIDARITY, organizado pela OMS e o RECOVERY, organizado pela Universidade de Oxford. Entretanto, foram nesses estudos clínicos, realizados com milhares de pessoas, que os resultados mostraram que a maioria dos remédios falharam.</p>
+                </blockquote>
+                <h2>Um pouco de calma, antes de sair tomando remédio nunca fez mal a ninguém…</h2>
+                <p>Mas por que não podemos tomar esses remédios, ao passo que eles constam na bula como seguros, mesmo sem um efeito comprovado? Primeiro que ser seguro, não quer dizer “de qualquer jeito” e “tomando enquanto eu estiver com vontade ou a pandemia durar” (o que acabar primeiro.</p>
+                <p>Segundo, que mesmo aqueles aprovados e vendidos em farmácias possuem uma dose e um tempo certo de administração. Por exemplo, você já reparou que todo remédio possui uma bula que relata efeitos adversos que podem ser simples, ou até mesmo muito graves?</p>
+                <p>Já existem relatos de pessoas que tiveram problemas de saúde decorrente do uso profilático de remédios sem prescrição médica, como por exemplo <strong>ivermectina e cloroquina.</strong> Em uma entrevista dada ao Jornal “O Globo”, o médico hepatologista Paulo Bittencourt informou que 27% das hepatites agudas graves ou fulminantes são de origem de medicamentos.</p>
+                <p>Além disso, outros problemas podem surgir, como por exemplo a falta desses remédios para quem realmente precisa tomar. Assim, cria-se uma falsa sensação de segurança, e as pessoas param de adotar medidas que realmente funcionam para o enfrentamento da pandemia, como uso de máscaras e distanciamento social. E também, investir em um remédio que não funciona significa que o dinheiro disponível para o combate a pandemia está sendo mal gasto.</p>
+                <p>Abaixo, preparamos uma lista com as principais tentativas de tratamento utilizadas durante esse primeiro ano de pandemia.</p>
+                <ul>
+                  <li>Hidroxicloroquina e Cloroquina.</li>
+                  <li>Ivermectina.</li>
+                  <li>Remdesivir.</li>
+                </ul>
+                <p>Ainda temos mais esses 3 tópicos.</p>
+                <ol>
+                  <li>Lopinavir e Ritonavir.</li>
+                  <li>Azitromicina.</li>
+                  <li>Dexametasona.</li>
+                </ol>
+                <p>Seguimos adiante com mais informações...</p>
+              </article>
+
+              <article class="blog-post">
+                <h2 class="blog-post-title">Por fim, e o desenvolvimento de novos remédios?</h2>
+                <p class="blog-post-meta">Fevereiro 23, 2021</p>
+
+                <p>E porque não começar a falar sobre o desenvolvimento de novos remédios, que sejam específicos para a COVID-19? Graças à ciência de base, que estudou os mecanismos de infecção e a biologia do vírus, agora podemos desenvolver remédios que funcionem efetivamente contra a COVID-19. Em suma, nos resta esperar os resultados de várias pesquisas que ainda estão sendo desenvolvidas!</p>
+              </article>
+              <nav class="blog-pagination mb-2" aria-label="Pagination">
+                <a class="btn btn-outline-primary" href="#">Antigas</a>
+                <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Novas</a>
+              </nav>
+            </div>
+            <div class="col-md-4">
+              <div class="p-4 mb-3 bg-light rounded">
+                <h4 class="fst-italic">Vacinação</h4>
+                <p class="mb-0">A Vacinação contra o corona vírus já começou, os idosos e trabalhadores da saúde serão os primeiros a serem vacinados nos postos de saúde do SUS.</p>
+              </div>
+              <div class="p-4">
+                <h4 class="fst-italic">Artigos</h4>
+                <ol class="list-unstyled mb-0">
+                  <li><a href="#">Março 2021</a></li>
+                  <li><a href="#">Fevereiro 2021</a></li>
+                  <li><a href="#">Janeiro 2021</a></li>
+                  <li><a href="#">Dezembro 2020</a></li>
+                  <li><a href="#">Novembro 2020</a></li>
+                  <li><a href="#">Outubro 2020</a></li>
+                  <li><a href="#">Setembro 2020</a></li>
+                  <li><a href="#">Agosto 2020</a></li>
+                  <li><a href="#">Julho 2020</a></li>
+                  <li><a href="#">Junho 2020</a></li>
+                  <li><a href="#">Maio 2020</a></li>
+                  <li><a href="#">Abril 2020</a></li>
+                </ol>
+              </div>
+            </div>
+          </div>
+        </main>
+      <div class="container-fluid" style="background-color: #293d55">
+        <footer class="container py-3" style="background-color: #293d55">
+          <div class="row">
+            <div class="col-6 col-md">
+              <h5 class="text-light">Features</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="link-secondary" href="#">Segurança</a></li>
+                <li><a class="link-secondary" href="#">Web</a></li>
+                <li><a class="link-secondary" href="#">Firewall</a></li>
+                <li><a class="link-secondary" href="#">Para desenvolvedores</a></li>
+                <li><a class="link-secondary" href="#">Data center</a></li>
+                <li><a class="link-secondary" href="#">Link up</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5 class="text-light">Resources</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="link-secondary" href="#">Planos</a></li>
+                <li><a class="link-secondary" href="#">Contatos</a></li>
+                <li><a class="link-secondary" href="#">Sobre</a></li>
+                <li><a class="link-secondary" href="#">Compras</a></li>
+              </ul>
+              </div>
+              <div class="col-6 col-md">
+                <h5 class="text-light">About</h5>
+                <ul class="list-unstyled text-small">
+                  <li><a class="link-secondary" href="#">Equipe</a></li>
+                  <li><a class="link-secondary" href="#">Localizações</a></li>
+                  <li><a class="link-secondary" href="#">Privacidade</a></li>
+                  <li><a class="link-secondary" href="#">Termos</a></li>
+                </ul>
+              </div>
+              <div class="col-6 col-md ">
+                <h5 class="text-light">Social networks</h5>
+                <ul class="list-inline text-small">
+                  <li class="list-inline-item"><a class="link-secondary " href="#" title="Facebook"><img src="facebook.png" class="icon"></a></li>
+                  <li class="list-inline-item"><a class="link-secondary " href="#" title="Instagram"><img src="instagram.png" class="icon"></a></li>
+                  <li class="list-inline-item"><a class="link-secondary " href="#" title="Whatsapp"><img src="wpp.png" class="icon"></a></li>
+                  <li class="list-inline-item"><a class="link-secondary " href="#" title="Twitter"><img src="twitter.png" class="icon"></a></li>
+                </ul>
+              </div>
+          </div>
+      </div>
+      <div class="container-fluid mt-auto py-3 bg-dark">
+        <div class="container">
+          <p class="text-light text-center p-2"><small>Wit Solutions CRM. | CNPJ 54.321.458/0001-03</small></p>
+          <p class="text-light text-center p-2"><small>Av. Tietê 2141, Torre E 10º andar - São paulo</small></p>
+          <p class="text-light text-center p-2"><small>Formas de pagamento aceitas: cartões de crédito (Visa, MasterCard, Elo e American Express) e boleto.</small></p>
+          <div class="container">
+            <hr class="featurette-divider bg-light">
+          </div>
+          <p class="text-light text-center p-2"><small>&copy 2021, todos os direitos reservados</small></p>
+        </div>
+      </div>
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
