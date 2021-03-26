f2e9a2276205570cf5c98f95f0ee3fad4b5241ee
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-03-26 13:52:14 -0300

    Create sobre.php

---
+++ sobre.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Wit Solutions | Contato</title>
+    <meta name="viewport" content="width=device-width">
+    <meta name="description" content="Wit Solutions empresa de web segurança, sua empresa está segura?">
+    <meta property="og:image" content="logo1.0.png">
+    <meta property="og:description" content="Wit Solutions empresa de web segurança, sua empresa está segura?">
+    <meta property="og:title" content="Wit Solutions">
+    <link rel="stylesheet" href="home.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <link rel="shortcut icon" href="logo2.0.png"/>
+  </head>
+  <body>
+    <header class="sticky-top">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid menu">
+          <a class="navbar-brand" href="index.php" title="Home"><img src="sec.png"></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="sobre.php">Sobre</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="contato.php">Contato</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#">Serviços e suporte</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="planos.php">Planos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="covid-19.php">COVID-19</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+      <div class="container">
+        <div class="row">
+          <div class="col-6 col-sm-3">
+            <h1>
+              Descrição da 
+              <span class="text-muted">empresa</span>
+            </h1>
+            <p class="lead">Wit Solutions é uma empresa de tecnologia e informações que visa como objetivo principal
+              manter a segurança de empresas com seus clientes através da web segurança com
+              softwares avançados de fácil entendimento e implantação rápida e justa que cabe no seu
+              bolso, suporte 24Hrs e compatível com todas as plataformas.</p>
+          </div>
+          <div class="col-6 col-sm-3">
+            <h1>
+              Nossa 
+            <span class="text-muted">missão</span>
+            </h1>
+            <p class="lead">Trazer as melhores soluções aos problemas requeridos das empresas através da web
+              segurança, utilizando de softwares avançados de fácil entendimento e rápida implantação
+              com preços acessíveis e indisputáveis no mercado.</p>
+          </div>
+          <div class="col-6 col-sm-3">
+            <h1>
+              Nossa 
+              <span class="text-muted">visão</span>
+            </h1>
+            <p class="lead">Tornar-se uma empresa de grande porte proporcionando as melhores experiências com a
+              empresa contratante ao seu cliente final, visando ser a melhor empresa com uma
+              interatividade única.</p>
+          </div>
+          <div class="col-6 col-sm-3">
+            <h1>
+              Nossos 
+              <span class="text-muted">Valores</span>
+            </h1>
+            <p class="lead">Manter a segurança e integridade de empresas com seus clientes através da web
+              segurança, com suporte de 24Hrs em qualquer plataforma com os melhores profissionais
+              disponíveis no mercado de IT (Inteligência & Tecnologia).</p>
+          </div>
+        </div>
+      </div>
+      </div>
+	<div class="container">
+		<hr>
+	</div>
+      <div class="container-fluid" style="background-color: #293d55">
+        <footer class="container py-3" style="background-color: #293d55">
+          <div class="row">
+            <div class="col-6 col-md">
+              <h5 class="text-light">Features</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="link-secondary" href="#">Segurança</a></li>
+                <li><a class="link-secondary" href="#">Web</a></li>
+                <li><a class="link-secondary" href="#">Firewall</a></li>
+                <li><a class="link-secondary" href="#">Para desenvolvedores</a></li>
+                <li><a class="link-secondary" href="#">Data center</a></li>
+                <li><a class="link-secondary" href="#">Link up</a></li>
+              </ul>
+            </div>
+            <div class="col-6 col-md">
+              <h5 class="text-light">Resources</h5>
+              <ul class="list-unstyled text-small">
+                <li><a class="link-secondary" href="#">Planos</a></li>
+                <li><a class="link-secondary" href="#">Contatos</a></li>
+                <li><a class="link-secondary" href="#">Sobre</a></li>
+                <li><a class="link-secondary" href="#">Compras</a></li>
+              </ul>
+              </div>
+              <div class="col-6 col-md">
+                <h5 class="text-light">About</h5>
+                <ul class="list-unstyled text-small">
+                  <li><a class="link-secondary" href="#">Equipe</a></li>
+                  <li><a class="link-secondary" href="#">Localizações</a></li>
+                  <li><a class="link-secondary" href="#">Privacidade</a></li>
+                  <li><a class="link-secondary" href="#">Termos</a></li>
+                </ul>
+              </div>
+              <div class="col-6 col-md ">
+                <h5 class="text-light">Social networks</h5>
+                <ul class="list-inline text-small">
+                  <li class="list-inline-item"><a class="link-secondary " href="#" title="Facebook"><img src="facebook.png" class="icon"></a></li>
+                  <li class="list-inline-item"><a class="link-secondary " href="#" title="Instagram"><img src="instagram.png" class="icon"></a></li>
+                  <li class="list-inline-item"><a class="link-secondary " href="#" title="Whatsapp"><img src="wpp.png" class="icon"></a></li>
+                  <li class="list-inline-item"><a class="link-secondary " href="#" title="Twitter"><img src="twitter.png" class="icon"></a></li>
+                </ul>
+              </div>
+          </div>
+      </div>
+      <div class="container-fluid mt-auto py-3 bg-dark">
+        <div class="container">
+          <p class="text-light text-center p-2"><small>Wit Solutions CRM. | CNPJ 54.321.458/0001-03</small></p>
+          <p class="text-light text-center p-2"><small>Av. Tietê 2141, Torre E 10º andar - São paulo</small></p>
+          <p class="text-light text-center p-2"><small>Formas de pagamento aceitas: cartões de crédito (Visa, MasterCard, Elo e American Express) e boleto.</small></p>
+          <div class="container">
+            <hr class="featurette-divider bg-light">
+          </div>
+          <p class="text-light text-center p-2"><small>&copy 2021, todos os direitos reservados</small></p>
+        </div>
+      </div>
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
