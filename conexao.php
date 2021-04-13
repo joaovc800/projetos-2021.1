7bf48f615b2fcfd18960506f6e8e96bc790e835f
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-04-13 18:49:58 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,19 +1,9 @@
 <?php
-//$host="us-cdbr-east-03.cleardb.com";
-//$port=3306;
-//$socket="";
-//$user="be8ee029d0a7f0";
-//$password="634edcf8";
-//$dbname="heroku_3f91cda5aaca95a";
+$host="us-cdbr-east-03.cleardb.com";
+$port=3306;
+$socket="";
+$user="be8ee029d0a7f0";
+$password="634edcf8";
+$dbname="heroku_3f91cda5aaca95a";
 
-//$conexao = mysqli_connect($host, $user, $password, $dbname, $port, $socket)
-	//or die ('Não foi possivel conectar com o banco' . mysqli_connect_error());
-
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["us-cdbr-east-03.cleardb.com"];
-$username = $url["be8ee029d0a7f0"];
-$password = $url["634edcf8"];
-$db = $url["heroku_3f91cda5aaca95a"];
-
-$conexao = mysqli_connect($server,$username, $password, $db);
+$conexao = mysqli_connect($host, $user, $password, $dbname, $port, $socket) or die ('Não foi possivel conectar com o banco' . mysqli_connect_error());
