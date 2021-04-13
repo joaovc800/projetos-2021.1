fddc3f9981da769c24336482ca128a07f26dafd9
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-04-12 21:08:51 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,6 +2,6 @@
 $host = 'us-cdbr-east-03.cleardb.com';
 $usuario = 'be8ee029d0a7f0';
 $senha = '634edcf8';
-$db = 'WIT-solutions';
+$db = 'heroku_3f91cda5aaca95a';
 
 $conexao = mysqli_connect($host,$usuario,$senha,$db) or die ('Não foi possivel conectar com o banco');
