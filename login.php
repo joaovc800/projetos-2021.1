bdadfac8aba6b8b37d855642268cc60dafa76fbf
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-04-13 18:21:11 -0300

    Update login.php

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
 <?php
-include('conexao.php');
 session_start();
+include('conexao.php');
 
 if(empty($_POST['usuario']) || empty($_POST['senha'])){
     header('Location: index.php');
@@ -10,14 +10,15 @@ if(empty($_POST['usuario']) || empty($_POST['senha'])){
 $usuario = mysqli_real_escape_string($conexao,$_POST['usuario']); // primeiro puxa a conexao depois o usuario e proteje contra SQL injection
 $senha = mysqli_real_escape_string($conexao,$_POST['senha']); // primeiro puxa a conexao depois a senha e proteje contra SQL injection
 
-$queryBuscarUsuario = "SELECT * FROM usuarios WHERE username = '{$usuario}' AND senha = MD5('{$senha}');";
+$queryBuscarUsuario = "SELECT username FROM usuarios WHERE username = '{$usuario}' AND senha = MD5('{$senha}');";
 
 $resultado = mysqli_query($conexao,$queryBuscarUsuario); // abre a conexão e execulta a query
 
 $row = mysqli_num_rows($resultado); // se encontrar o resultado ela vai retornar true que é igual à 1
 
 if($row == 1){
-    $_SESSION['usuario'] = $usuario; //autenticado armazena a sessão na variavel usuario
+    $usuario_bd = mysqli_fetch_assoc($resultado);
+    $_SESSION['usuario'] = $usuario_bd['usuario']; //autenticado armazena a sessão na variavel usuario
     header('Location: menu.php'); //leva para pagina desejada
     exit(); // fecha os cabeçalhos
 }
