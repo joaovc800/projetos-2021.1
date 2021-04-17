a17ccca1c2cf5b772c74c545ea14d810a55fcc77
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-04-17 11:57:44 -0300

    Update criarconta.php

--- criarconta.php
+++ criarconta.php
@@ -1,80 +1,81 @@
+<?php
+session_start()
+?>
+
 <!DOCTYPE html>
-<html lang="pt-BR">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<html>
+    
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Cadastrar</title>
-    <link rel="stylesheet" href="style.css" />
-    <script src="script.js"></script>
-  </head>
-  <body>
-    <section class="form-section">
-      <h1>Cadastre-se</h1>
-      <div class="form-wrapper">
-      <?php
-        if($_SESSION['status_cadastro']):
-        ?>
-        <div>
-            <p>Cadastro efetuado!</p>
-            <p>Faça login informando o seu usuário e senha <a href="login.php">aqui</a></p>
-        </div>
-        <?php
-        endif;
-        unset($_SESSION['status_cadastro'])
-        ?>
-        <?php
-        if($_SESSION['usuario_existe']):
-        ?>
-        <div>
-            <p>O usuário escolhido já existe. Informe outro e tente novamente.</p>
-        </div>
-        <?php
-        endif;
-        unset($_SESSION['usuario_existe'])
-        ?>
-        <?php
-        if($_SESSION['usuario_invalido']):
-        ?>
-        <div>
-            <p>Nenhum campo pode ficar vázio</p>
-        </div>
-        <?php
-        endif;
-        unset($_SESSION['usuario_invalido'])
-        ?>
-        <?php
-        if($_SESSION['senha_nao_corresponde']):
-        ?>
-        <div>
-            <p>Senhas não correspondem</p>
-        </div>
-        <?php
-        endif;
-        unset($_SESSION['senha_nao_corresponde'])
-        ?>
-        <form action="cadastrar.php" method="POST">
-            <div class="input-block">
-                <label for="login-email">Nome completo</label>
-                <input name="usuario" type="text" id="login-email" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Sistema de Cadastro</h3>
+                    <?php
+                    if($_SESSION['status_cadastro']):
+                    ?>
+                    <div class="notification is-success">
+                      <p>Cadastro efetuado!</p>
+                      <p>Faça login informando o seu usuário e senha <a href="login.php">aqui</a></p>
+                    </div>
+                    <?php
+                    endif;
+                    unset($_SESSION['status_cadastro'])
+                    ?>
+                    <?php
+                    if($_SESSION['usuario_existe']):
+                    ?>
+                    <div class="notification is-info">
+                        <p>O usuário escolhido já existe. Informe outro e tente novamente.</p>
+                    </div>
+                    <?php
+                    endif;
+                    unset($_SESSION['usuario_existe'])
+                    ?>
+                    <?php
+                    if($_SESSION['usuario_invalido']):
+                    ?>
+                    <div class="notification is-danger">
+                        <p>Nenhum campo pode ficar vázio</p>
+                    </div>
+                    <?php
+                    endif;
+                    unset($_SESSION['usuario_invalido'])
+                    ?>
+                    <div class="box">
+                        <form action="cadastrar.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input name="nome" type="text" class="input is-large" placeholder="Nome" autofocus>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="usuario" type="text" class="input is-large" placeholder="Usuário">
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Senha">
+                                </div>
+                            </div>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Cadastrar</button>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="input-block">
-                <label for="login-email">Usuário</label>
-                <input name="usuario" type="text" id="login-email" />
-            </div>
-            <div class="input-block">
-                <label for="login-password">Senha</label>
-                <input name="senha1" type="password" id="login-password" />
-            </div>
-            <div class="input-block">
-                <label for="login-password">Senha</label>
-                <input name="senha2" type="password" id="login-password" />
-            </div>
-          <button type="submit" class="btn-login">Cadastrar</button>
-          <a href="index.php">Voltar</a>
-        </form>
-      </div>
+        </div>
     </section>
-    <ul class="squares"></ul>
-  </body>
+</body>
+
 </html>
