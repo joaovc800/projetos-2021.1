9f6590352ec3f05b0967ddf87c8f5615834577e9
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-04-19 19:43:04 -0300

    Update criarconta.php

--- criarconta.php
+++ criarconta.php
@@ -13,6 +13,7 @@ session_start()
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
     <link rel="stylesheet" href="css/bulma.min.css" />
     <link rel="stylesheet" type="text/css" href="css/login.css">
+    <link rel="shortcut icon" href="wit.png"/>
 </head>
 
 <body>
