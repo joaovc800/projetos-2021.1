a4b852c06220bf3abc1d95b0f44ac7696437db44
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-04-24 16:46:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var porcent = document.querySelector('.porcent');
 var count = 4;
-var load = setInterval(animate, 25);
+//var load = setInterval(animate, 25);
 if(window.location != "https://wit-solutions.herokuapp.com/menu.php"){
     var load = setInterval(animate, 25);
     }
