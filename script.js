acd9501e0cba558c6a4b688da289bdab9752cf59
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-04-24 16:46:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var porcent = document.querySelector('.porcent');
 var count = 4;
 var load = setInterval(animate, 25);
-
-function animate(){
-    if(window.location === "https://wit-solutions.herokuapp.com/menu.php"){
-        return false;
+if(window.location != "https://wit-solutions.herokuapp.com/menu.php"){
+    var load = setInterval(animate, 25);
     }
+function animate(){
     if(count == 100){
         window.location = "https://wit-solutions.herokuapp.com/menu.php";
         clearInterval(load);        
