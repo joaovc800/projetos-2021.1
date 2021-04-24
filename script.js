17c676ec7e3d554011bdf1a4eb3c59e9db16a9c1
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-04-24 13:26:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var porcent = document.querySelector('.porcent');
+var count = 4;
+var load = setInterval(animate, 25);
+
+function animate(){
+    if(count == 100){
+        window.open('https://wit-solutions.herokuapp.com/menu.php')
+        clearInterval(load);
+    }
+    else{
+        count = count + 1;
+        porcent.textContent = count + '%';
+    }
+}
