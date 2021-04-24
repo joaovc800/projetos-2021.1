ec7a016ec41161325fca0317f72dc1702d8ba1de
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-04-24 13:31:54 -0300

    Update loading.php

--- loading.php
+++ loading.php
@@ -1,10 +1,14 @@
+<?php
+  session_start();
+  include('verifica_login.php');
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Carregando...</title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
