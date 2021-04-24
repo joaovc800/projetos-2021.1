a3f6f08fbd02d14fcfe08e1e24c563a95245c670
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-04-24 13:54:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ var load = setInterval(animate, 25);
 
 function animate(){
     if(count == 100){
-        clearInterval(load);
-        while(count == 100){
+          while(count == 100){
             window.location.href = "https://wit-solutions.herokuapp.com/menu.php";
             break;
         }
+        clearInterval(load);
         
     }
     else{
