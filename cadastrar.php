06eff3084b3171ab7db25ab264e7ec1ef937aa76
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-05-08 22:07:12 -0300

    Update cadastrar.php

--- cadastrar.php
+++ cadastrar.php
@@ -7,7 +7,7 @@ if(empty($_POST['nome']) || empty($_POST['usuario']) || empty($_POST['senha1']) 
 	header('Location: criarconta.php');
     exit();
 }
-$nome = mysqli_real_escape_string($conexao,utf8_encode($_POST['nome']);
+$nome = mysqli_real_escape_string($conexao,$_POST['nome']);
 $telefone = mysqli_real_escape_string($conexao,($_POST['telefone']));
 $usuario = mysqli_real_escape_string($conexao, trim($_POST['usuario']));
 $senha1 = mysqli_real_escape_string($conexao, trim($_POST['senha1']));
