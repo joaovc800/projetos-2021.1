b00bbbaccd5de937a9a4e2a311f1679365027ae3
Borba <gabriel.404e@gmail.com>
2021-05-21 18:21:32 -0300

    f

--- conexao.php
+++ conexao.php
@@ -5,3 +5,4 @@ $password="634edcf8";
 $dbname="heroku_3f91cda5aaca95a";
 
 $conexao = mysqli_connect($host, $user, $password, $dbname) or die ('Não foi possivel conectar com o banco•' . mysqli_connect_error());
+?>
