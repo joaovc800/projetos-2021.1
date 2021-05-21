2203e92f721dc52700335604497554b9dc68f570
Borba <gabriel.404e@gmail.com>
2021-05-21 18:51:03 -0300

    f

--- aprovacao.php
+++ aprovacao.php
@@ -2,35 +2,36 @@
   session_start();
   include('verifica_login.php');
 ?>
-<?php
-  include("conexao.php");
-  $plano = $_COOKIE['plano'];
-  if($plano == '1') {
-    $uid = "Community";
-    $valor = '50.00';
-    setcookie("plano", "", time() - 3600);
-  }
-  if($plano == '2') {
-    $uid = "Pro";
-    $valor = '120.00';
-    setcookie("plano", "", time() - 3600);
-  }
-  if($plano == '3') {
-    $uid = "Enterprise";
-    $valor = '200.00';
-    setcookie("plano", "", time() - 3600);
-  }
-  if($uid != NULL) {
-    $queryPacote = "INSERT INTO `heroku_3f91cda5aaca95a`.`planos` (id_planos,plano,status,valor) VALUES(NULL,'$uid','ativo','$valor')";
-    $resultado = mysqli_query($conexao,$queryPacote);
-    $linha = mysqli_fetch_assoc($resultado);
-  }
-  // header('Location: aprovacao.php');
-  // exit;
-?>
+
 <!DOCTYPE html>
 <html>
   <head>
+    <?php
+    include("conexao.php");
+    $plano = $_COOKIE['plano'];
+    if($plano == '1') {
+      $uid = "Community";
+      $valor = '50.00';
+      setcookie("plano", "", time() - 3600);
+    }
+    if($plano == '2') {
+      $uid = "Pro";
+      $valor = '120.00';
+      setcookie("plano", "", time() - 3600);
+    }
+    if($plano == '3') {
+      $uid = "Enterprise";
+      $valor = '200.00';
+      setcookie("plano", "", time() - 3600);
+    }
+    if($uid != NULL) {
+      $queryPacote = "INSERT INTO `heroku_3f91cda5aaca95a`.`planos` (id_planos,plano,status,valor) VALUES(NULL,'$uid','ativo','$valor')";
+      $resultado = mysqli_query($conexao,$queryPacote);
+      $linha = mysqli_fetch_assoc($resultado);
+    }
+    // header('Location: aprovacao.php');
+    // exit;
+    ?>
     <meta charset="utf-8">
     <title>Wit Solutions | Aprovação</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
@@ -43,7 +44,7 @@
     <script src="script.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script> -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
     <link rel="shortcut icon" href="wit.png"/>
   </head>
   <body>
@@ -141,6 +142,6 @@
           <p class="text-light text-center p-2"><small>&copy 2021, todos os direitos reservados</small></p>
         </div>
       </div>
-    <!-- <script src="../assets/dist/js/bootstrap.bundle.min.js"></script> -->
+    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
   </body>
 </html>
