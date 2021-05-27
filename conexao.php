5d6163f3cd3d530af1416ef655c2a9c263472cbe
João Vinicius da Costa <73707213+joaovc800@users.noreply.github.com>
2021-05-27 16:45:54 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -5,4 +5,10 @@ $password="634edcf8";
 $dbname="heroku_3f91cda5aaca95a";
 
 $conexao = mysqli_connect($host, $user, $password, $dbname) or die ('Não foi possivel conectar com o banco•' . mysqli_connect_error());
+
+// arruma a acentuação
+mysqli_query($conexao,"SET NAMES 'utf8'");  
+mysqli_query($conexao,'SET character_set_connection=utf8');
+mysqli_query($conexao,'SET character_set_client=utf8');  
+mysqli_query($conexao,'SET character_set_results=utf8'); 
 ?>
